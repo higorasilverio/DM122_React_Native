923c1339ba4bdac140c8ee1f9c77654cac170d09
higorasilverio <higorasilverio@outlook.com>
2020-12-12 19:54:03 -0300

    Insert style on profile component

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-// import style from './style'
+import style from './style'
 
 const Profile = () => {
     return (
-        <View>
-            <Text>Profile</Text>
+        <View style={style.container}>
+            <Text style={style.item}>Profile</Text>
         </View>
     )
 }
